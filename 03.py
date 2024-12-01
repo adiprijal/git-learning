a29e7e1304d10539eb3db6715d1f82afe5da102e
Adip Rijal <rizalaryan2@gmail.com>
2024-12-01 22:20:04 +0545

    commit 3

---
+++ 03.py
@@ -0,0 +1,16 @@
+'''
+
+    create a file named as ".gitignore"
+    inside your git containing directory 
+
+    It tells git which files/folders to not track
+
+    just write the name of the file or folders inside .gitignore to not track changes in those files
+
+    Ex: 
+    # .gitignore                    { inside .gitignore }
+        git-sheet.pdf
+
+        this code stop the tracking of git-sheet.pdf
+        
+'''
