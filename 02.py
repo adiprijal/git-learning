08ed6fd99938516b1aac269b0c210f1889835990
Adip Rijal <rizalaryan2@gmail.com>
2024-12-01 22:09:54 +0545

    commit 2

---
+++ 02.py
@@ -0,0 +1,21 @@
+'''
+ 
+    version = commit 
+    Version history = commit history
+
+    Download git:
+    https://git-scm.com/downloads
+
+    git init                                Git will start tracking all changes in the current folder 
+    git status                              Show all changes since the previous commit
+
+
+    git add  <file|folder>                  Pick changes to go into next commit 
+        git add  file                           Pick individual file 
+        git add  folder                         Pick all files inside a folder (and subfolders) 
+        git add  .                              Pick all files (in folder command line is running in) 
+
+    git commit  -m "message"                Creates a commit with a message attached 
+    git commit  -m "message" --amend        Update previous commit instead of creating new one 
+
+'''
