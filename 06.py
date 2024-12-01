5aeba3063e4a3fdad859d56fcd04dd229cbd268e
Adip Rijal <rizalaryan2@gmail.com>
2024-12-01 22:57:56 +0545

    commit 6

---
+++ 06.py
@@ -0,0 +1,46 @@
+'''
+
+    >> commit using VSC <<
+
+    Some code editor provides git integration feature like VSCode
+
+    this feature makes it easier to use git without having to use 'Terminal' to write git commands
+
+    I use VSCode.
+    It has git integration.
+
+    On the left side of vscode , there is a button named ' Source Control '
+
+    This button is git using area.
+
+
+    In 'Changes' section , all changes after last add are shown.
+    These are the changes that has still not been added or commited.
+
+    click '+' sign of the respective file you want to add to new commit
+    This is similar to using 
+                git add <file>
+
+    Click '-' to bring the file back to 'Changes' from 'Staged Changes'
+    Similar to using
+                git reset <file>
+
+    After adding, changed file moves to 'Staged Changes' section
+
+    Staging Area  = contains changes that will go into the next commit 
+
+    write message in message box and click 'Commit'
+    >NEW COMMIT IS CREATED<
+
+    Similar to using
+                git commit --m "message"
+
+    You can also amend by clicking 'Commit(Amend)' button
+                git commit --m "message" --amend
+
+
+                
+    You can also compare changes, commits or discard changes.
+    You can also see the lists of your commits.
+    
+'''
