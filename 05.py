17dbab09ca3325b41f2b66ff249004b7b9fe2741
Adip Rijal <rizalaryan2@gmail.com>
2024-12-01 22:33:46 +0545

    commit 5

---
+++ 05.py
@@ -0,0 +1,8 @@
+'''
+
+    git log                         View the commit history    
+        git log  --all                  Show all commits (not just current branch)
+        git log  --all --graph          Show branching visually in the command line
+ 
+ 
+'''
