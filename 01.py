76bc3c08d87787506f3f4c7b476143a4d27acb57
Adip Rijal <rizalaryan2@gmail.com>
2024-12-01 22:30:40 +0545

    commit 4

--- 01.py
+++ 01.py
@@ -1,1 +1,3 @@
 print("Hello World")
+
+# all git commands are run inside command prompt or terminal 
---
+++ 04.py
@@ -0,0 +1,10 @@
+'''
+    if error occurs asking for your name and email
+
+
+    >> Configure Name & Email for Commits <<
+    
+        git config  --global user.name  "Your Name" 
+        git config  --global user.email  "email@example.com" 
+
+'''
