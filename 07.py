1bfa796ec54c711a308d07fd4ad0bcc9eb12b2d6
Adip Rijal <rizalaryan2@gmail.com>
2024-12-02 15:55:05 +0545

    commit 7

---
+++ 07.py
@@ -0,0 +1,14 @@
+'''
+
+    git reset  <file|folder>            staging -> working
+        git reset  file 
+        git reset  folder
+        git reset .
+
+
+    git checkout --  <file|folder>      working => remove the  changes 
+        git checkout --  file 
+        git checkout --  folder/ 
+        git checkout --  . 
+
+'''
