123ea5a5ccd5bc134956d20402ba75c4061e0e76
Adip Rijal <rizalaryan2@gmail.com>
2024-12-02 17:43:56 +0545

    commit 10

---
+++ 10.py
@@ -0,0 +1,15 @@
+# Commit 10
+
+'''
+
+    >> Restoring to previous commit <<
+    git checkout <hash>       will only view previous commit
+
+    To restore previous version, we use
+
+    git checkout <hash|branch> <file|folder>
+        git checkout <hash|branch> file
+        git checkout <hash|branch> folder
+        git checkout <hash|branch> .
+
+'''
