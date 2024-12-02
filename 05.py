bec5cbb15227725affe80741f8df09a700f2c9d6
Adip Rijal <rizalaryan2@gmail.com>
2024-12-02 18:30:20 +0545

    commit 12

--- 05.py
+++ 05.py
@@ -2,6 +2,8 @@
 
 '''
 
+    >> Viewing our commits <<
+
     git log                         View the commit history    
         git log  --all                  Show all commits (not just current branch)
         git log  --all --graph          Show branching visually in the command line
---
+++ 12.py
@@ -0,0 +1,8 @@
+# Commit 12
+
+''' 
+    >> Removing git from project <<
+
+    rm -rf .git    
+    
+'''
