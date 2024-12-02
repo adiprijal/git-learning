79d8bddffe28ab2d495f8645f0750005db94c266
Adip Rijal <rizalaryan2@gmail.com>
2024-12-02 18:26:28 +0545

    commit 11

---
+++ 11.py
@@ -0,0 +1,24 @@
+# Commit 11
+
+'''
+
+    >> creating alias for git command <<                alias - shortcut
+
+        sometimes writing same command time and time may be hectic
+        so you can create a alias for your command
+
+        git config --global alias.shortcut <command>
+        
+        Ex:     git config --global alias.a add
+                git config --global alias.s status
+                git config --global alias.c commit 
+
+                Now,
+                        git commit --m "message"
+                and,    git c --m "message"
+                    does same thing
+                
+        # Remember both command works  { original & alias }
+            
+
+'''
