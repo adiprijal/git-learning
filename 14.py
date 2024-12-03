039f8ec9aad7228dc84de2c9349890cf553ab429
Adip Rijal <rizalaryan2@gmail.com>
2024-12-03 19:34:05 +0545

    commit 14

---
+++ 14.py
@@ -0,0 +1,15 @@
+# Commit 14
+
+'''
+
+    >> GitHub <<                        similar : BitBucket, GitLab
+
+    a service that lets us save our git repositories online. It also helps us: -   backup our code in case we delete it on our computer 
+    -   see the history of our code changes more easily
+
+    Why use github?
+    -   Github allows us to save our code in the internet along with its commit history.
+    -   It is useful while working on a project in group.
+    -   It allows multiple users to view, comment, pull(download) and push(upload) allowing multiple users to work on same project or different portion of it and push their version of code as commits.
+
+'''
