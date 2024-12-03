3adbea2247bf9ba7281ba8c2c004281182f6db36
Adip Rijal <rizalaryan2@gmail.com>
2024-12-03 19:42:56 +0545

    commit 15

---
+++ 15.py
@@ -0,0 +1,21 @@
+# Commit 15
+
+'''
+
+    Now we'll learn to use GitHub.
+
+    Repository = a folder containing code where any changes to the code are tracked by git. 
+    (To create a repository, we create a new folder on our computer, and then run  git init 
+
+    local repository - repository in your computer
+    remote repository - repository in internet i.e. GitHub
+
+    >> GitHub<<
+
+    ~ Download GitHub or go to  www.github.com
+    ~ Create an id
+    ~ Create a new repository       [ repository - folder ]
+            Ex: https://github.com/adiprijal/git-learning
+    
+
+'''
