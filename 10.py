200a71eb50cd127f4cc772751803fa13a741d5d3
Adip Rijal <rizalaryan2@gmail.com>
2024-12-03 17:34:59 +0545

    commit 13

--- 10.py
+++ 10.py
@@ -3,6 +3,13 @@
 '''
 
     >> Restoring to previous commit <<
+
+    Why?
+
+    When we are doing any project we may find out that our code was better and accurate in any previous commit.
+    In that case, we'll want to restore that code.
+
+
     git checkout <hash>       will only view previous commit
 
     To restore previous version, we use
@@ -11,5 +18,17 @@
         git checkout <hash|branch> file
         git checkout <hash|branch> folder
         git checkout <hash|branch> .
+
+        What happens?
+        After this, your code from that commit hash will be restored in the latest commit.
+        
+        Know that it will not create a new commit. Rather it'll update the HEAD commit with the code of that commit which you wanted to restore. 
+
+        Also it only keeps the changes in staging area i.e. changes are only added , not commited.
+
+        So, now you can either 
+        1)  amend current HEAD commit
+        2)  create new commit for restored code
+
 
 '''
---
+++ 13.py
@@ -0,0 +1,17 @@
+# Commit 13
+
+'''
+
+    Let's talk more about restoring a commit.
+
+    When you make many commits, they will be tracked in the main branch or master branch.
+    But when you return back to any previous commit and decide to restore that commit, a new branch is formed.
+
+    This commit will be outside the master branch and any other commits after that will continue in that other branch.
+
+    Since this branch is auto generated and not created by yourself, is you return and restore a commit from master branch, the other branch will disappear from log graph. [log --all --graph]
+
+
+    To prevent that, we will learn to create branch in git.
+
+'''
