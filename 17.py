eb87547fb96a6db9e54fb2d68f6adb93d99485eb
Adip Rijal <rizalaryan2@gmail.com>
2024-12-03 20:56:55 +0545

    commit 17

---
+++ 17.py
@@ -0,0 +1,13 @@
+# Commit 17
+
+'''
+
+    Now, we have to config git with our GitHub username to be able to make changes in our remote repo.
+
+    git config --global credential.username <"github_username"> 
+
+        Ex :  git config --global credential.username "adiprijal"
+
+    
+
+'''
