ff597a60e9de1591f603f2e3be2af3e84d402d89
Adip Rijal <rizalaryan2@gmail.com>
2024-12-03 21:26:19 +0545

    commit 19

---
+++ 19.py
@@ -0,0 +1,15 @@
+# Commit 19
+
+'''
+
+    After pushing the main branch, now if you make a new commit in your local repo, it'll not sync directly to our remote repo.
+
+    In that case, you'll see HEAD -> master and origin/master in different commits.
+
+    Now we've to push the new commit to origin.
+
+    git push origin master
+
+    
+
+'''
