ca13c0a897568958c52bed674541deb3e6e5db8c
Adip Rijal <rizalaryan2@gmail.com>
2024-12-04 15:02:03 +0545

    commit 20

---
+++ 20.py
@@ -0,0 +1,24 @@
+# Commit 20
+
+'''
+
+    Now, lets learn to set a shortcut to do git push.
+    Until now, to git push we have to run
+        git push origin master
+
+    Now to reduce this command to only two words we do...
+
+    git push <remote_name> <branch> --set--upstream
+
+    Ex:     git push origin master --set--upstream
+        
+    Now, we have set ' origin master ' as default push setting for this repo.
+
+    Now, we can use command;
+            git push
+
+        This works exactly like git push origin master
+
+    Remember, both the commands works.
+
+'''
