f6addd802bca62a069894bdeff709866e757dbe5
Adip Rijal <rizalaryan2@gmail.com>
2024-12-04 21:56:41 +0545

    commit 24

---
+++ 24.py
@@ -0,0 +1,18 @@
+# Commit 24
+
+'''
+
+    >> Fetch remote repo <<
+
+    Manoj has already updated remote repo in GitHub but my terminal still doesn't know that .
+    SO, we'll fetch that updated version of remote repo.
+
+    Remember, this is useful when multiple users are working on a same project in different pc.
+
+    Command:
+            git fetch
+
+    Now, in Adip's pc, origin/master will be ahead of HEAD -> master.
+    Becoz he still hasn't pull those changes to his local repo.
+
+'''
