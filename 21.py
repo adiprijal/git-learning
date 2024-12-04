e31207f582676969e5ab9330e78e34bd662cf605
Adip Rijal <rizalaryan2@gmail.com>
2024-12-04 15:16:33 +0545

    commit 21

---
+++ 21.py
@@ -0,0 +1,16 @@
+# commit 21
+
+'''
+
+    >> Push Error <<
+
+    When you make a new commit in your local repo and then try to push
+    To https://github.com/adiprijal/git-learning.git
+    ! [rejected]        master -> master (non-fast-forward)
+    error: failed to push some refs to 'https://github.com/adiprijal/git-learning.git'
+    hint: Updates were rejected because the tip of your current branch is behind
+    hint: its remote counterpart. If you want to integrate the remote changes,
+    hint: use 'git pull' before pushing again.
+    hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+
+'''
